5f01879346ee8af9a8aafad4680c11315b673fe3
Anwesha Das <2023mt93350@wilp.bits-pilani.ac.in>
2024-04-20 16:47:06 +0530

    adding basic app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '',   redirectTo: 'landing', pathMatch: 'full' },
+    { path: 'landing', component: LandingComponent },
+    { path: 'home', component: HomeComponent }
+];
