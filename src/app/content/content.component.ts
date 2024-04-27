bc6b0130683e5fe48e35ad5f898ad4c5ebcee865
Anwesha Das <2023mt93350@wilp.bits-pilani.ac.in>
2024-04-27 18:47:05 +0530

    added user auth

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { AxiosService } from '../axios.service';
 import { LoginComponent } from '../login/login.component';
+import { HomeComponent } from '../home/home.component';
+
+import { CommonModule } from '@angular/common';
+import { LoginButtonComponent } from '../login-button/login-button.component';
+import { LogoutButtonComponent } from '../logout-button/logout-button.component';
+import { LoggedOutComponent } from '../logged-out/logged-out.component';
 
 @Component({
   selector: 'app-content',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, HomeComponent, CommonModule, LoginButtonComponent, LogoutButtonComponent, LoggedOutComponent],
   templateUrl: './content.component.html',
   styleUrl: './content.component.css'
 })
 export class ContentComponent {
 
+  componentToShow: string = "logged-out";
+
   constructor(private axiosService: AxiosService){}
+
+  showComponent(componentToShow: string){
+    this.componentToShow = componentToShow;
+  }
 
   onLogin(input: any): void{
     this.axiosService.requestData(
@@ -21,7 +33,10 @@ export class ContentComponent {
         login: input.login,
         password: input.password
       }
-    )
+    ).then(response => {
+      this.axiosService.setAuthToken(response.data.token);
+      this.componentToShow = "home";
+    });
   }
 
   onRegister(input: any): void{
@@ -34,7 +49,10 @@ export class ContentComponent {
         login: input.login,
         password: input.password
       }
-    )
+    ).then(response => {
+      this.axiosService.setAuthToken(response.data.token);
+      this.componentToShow = "home";
+    });
   }
 
 }
--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -5,11 +5,12 @@ import {MatTabsModule} from '@angular/material/tabs';
 import { HeaderComponent } from '../header/header.component';
 import { LoginComponent } from '../login/login.component';
 import { ContentComponent } from '../content/content.component';
+import { LoggedOutComponent } from '../logged-out/logged-out.component';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [MatButtonModule, MatCardModule, MatTabsModule, HeaderComponent, LoginComponent, ContentComponent],
+  imports: [MatButtonModule, MatCardModule, MatTabsModule, LoggedOutComponent, HeaderComponent, LoginComponent, ContentComponent],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.css'
 })
