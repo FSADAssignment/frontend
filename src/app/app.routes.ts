7fc4edfb0067ff02e18bd0d5bc98f1db7ee37bf0
Anwesha Das <2023mt93350@wilp.bits-pilani.ac.in>
2024-05-12 20:53:48 +0530

    adding routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
+import { AttemptTestComponent } from './attempt-test/attempt-test.component';
+import { TestResultComponent } from './test-result/test-result.component';
+import { TestsComponent } from './tests/tests.component';
 
 export const routes: Routes = [
     { path: '',   redirectTo: 'landing', pathMatch: 'full' },
     { path: 'landing', component: LandingComponent },
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'tests', component: TestsComponent },
+    { path: 'attempt', component: AttemptTestComponent },
+    { path: 'testResult', component: TestResultComponent }
 ];
